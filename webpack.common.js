93ee0b654f22f77d58647f02362f19b7c28e434a
Justice <122542069+goob8079@users.noreply.github.com>
2025-10-27 23:08:57 +0900

    webpack installed and some html and css setup

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    // disable performance warnings and increase allowed asset size
+    performance: {
+        hints: false,
+        maxEntrypointSize: 5120000,
+        maxAssetSize: 5120000
+    },
+};
